65623a4e69209059687c66ac468db984091b5e1a
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-08-01 16:02:17 +0600

    header top no adaptive

--- src/app/ui/components/header/Header.tsx
+++ src/app/ui/components/header/Header.tsx
@@ -1,18 +1,30 @@
 import Link from "next/link";
 import "./header.module.scss";
+import Image from "next/image";
+import logoImage from "@/assets/icons/logo.svg";
+import catalogIcon from "@/assets/icons/catalog_burger.svg";
+import searchIcon from "@/assets/icons/search_icon.svg";
+import myProfileIcon from "@/assets/icons/myProfile_icon.svg";
 
 export default function Header() {
   return (
     <>
-        <header>
+        <header className="header">
             <div className="header-top">
-                <div className="header__logo"></div>
-                <div className="header__catalog">Каталог</div>
-                <div className="header__search">
-                    <input type="text" placeholder="Поиск" />
-                    <button></button>
+                <div className="header-top-left">
+                    <div className="header__logo"><Image src={logoImage} alt="City 312"></Image></div>
+                    <div className="header__catalog">
+                        <div><Image src={catalogIcon} alt={""}></Image></div>
+                        <div>Каталог</div>
+                    </div>
                 </div>
-                <div className="header__user"></div>
+                <div className="header-top-right">
+                    <div className="header__search">
+                        <input className="search__input" type="text" placeholder="Поиск" />
+                        <button className="search__button"><Image src={searchIcon} alt={""}></Image></button>
+                    </div>
+                    <div className="header__user"><Image src={myProfileIcon} alt={""}></Image></div>
+                </div>
             </div>
             <div className="header-bottom">
                 <nav className="header__navbar">
