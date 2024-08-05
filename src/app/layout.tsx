86f6143f3e04abae47af156c0ed5191fd138442d
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2024-08-05 13:08:29 +0600

    Header desktop, header mobile devision, adaptive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,45 @@
+"use client"
 import "@/app/ui/global.scss";
 
 import "../../node_modules/bootstrap/scss/bootstrap.scss";
-import Header from "./ui/components/header/Header";
+import HeaderDesktop from "./ui/components/header/HeaderDesktop";
+import { useState, useEffect } from "react";
+import HeaderMobile from "./ui/components/header/HeaderMobile";
 
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    if (windowWidth === 0) {
+        return null; 
+    }
+
   return (
     <html lang="en">
       <body>
-        <Header></Header>
-          {children}
+        { windowWidth === 0 ? null : null}
+        { windowWidth > 768 ? <HeaderDesktop></HeaderDesktop> : <HeaderMobile></HeaderMobile>}
+        {children}
         <footer>footer</footer>
       </body>
     </html>
---
+++ src/app/ui/components/header/HeaderMobile.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Link from "next/link";
+import "./header.module.scss";
+import Image from "next/image";
+import logoImage from "@/assets/icons/logo-mobile.svg";
+import catalogIcon from "@/assets/icons/catalog_burger.svg";
+import searchIcon from "@/assets/icons/search_icon.svg";
+import myProfileIcon from "@/assets/icons/myProfile_icon.svg";
+import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+
+export default function HeaderMobile() {
+    
+
+    return (
+        <>
+            <header className="header header-mobile">
+                <div className="header-top">
+                    <Link href={"/"} className="header__catalog">
+                        <div><Image src={catalogIcon} alt={""}></Image></div>
+                    </Link>
+                    <div className="header__logo"><Image src={logoImage} alt="City 312"></Image></div>
+                    <Link href={"/profile"}className="header__user"><Image src={myProfileIcon} alt={""}></Image></Link>      
+                </div>
+                <div className="header-bottom">
+                    <nav className="header__navbar">
+                        <Link href="/">
+                            <div className="navbar__element">Афиши</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element">Акции</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element">Куда сходить</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element">Сертификаты</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element">Промокоды</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element">Для бизнеса</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="navbar__element navbar__question">Q & A</div>
+                        </Link>
+                    </nav>
+                    <div className="header__search">
+                        <input className="search__input" type="text" placeholder="Поиск" />
+                        <button className="search__button"><Image src={searchIcon} alt={""}></Image></button>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
